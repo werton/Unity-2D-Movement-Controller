12cee6464e89c8ff38ed2174ee43648b6de91d1c
werton <weerbox@gmail.com>
2022-10-13 23:44:14 +0900

    Moved to new Input System

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,32 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerVelocity))]
 public class PlayerInput : MonoBehaviour
 {
+    private PlayerVelocity playerVelocity;
+    public InputControl Input { get; private set; }
 
-	private PlayerVelocity playerVelocity;
+    private void Awake()
+    {
+        Input = new InputControl();
+        Input.Player.Jump.performed += OnKeyJump;
+        Input.Player.Move.performed += OnKeyMove;
+        Input.Player.Move.canceled += OnKeyMove;
+    }
 
-	void Start()
-	{
-		playerVelocity = GetComponent<PlayerVelocity>();
-	}
+    private void Start()
+    {
+        playerVelocity = GetComponent<PlayerVelocity>();
+    }
 
-	void Update()
-	{
-		Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		playerVelocity.SetDirectionalInput(directionalInput);
+    private void OnEnable()
+    {
+        Input.Enable();
+    }
 
-		if (Input.GetKeyDown(KeyCode.W))
-		{
-			playerVelocity.OnJumpInputDown();
-		}
-		if (Input.GetKeyUp(KeyCode.W))
-		{
-			playerVelocity.OnJumpInputUp();
-		}
-		if (Input.GetKeyDown(KeyCode.S))
-		{
-			playerVelocity.OnFallInputDown();
-		}
-	}
+    private void OnDisable()
+    {
+        Input.Disable();
+        Input.Player.Jump.performed -= OnKeyJump;
+        Input.Player.Move.performed -= OnKeyMove;
+        Input.Player.Move.canceled -= OnKeyMove;
+    }
+
+    public void OnKeyMove(InputAction.CallbackContext context)
+    {
+        var horizontalDirection = context.ReadValue<Vector2>();
+
+        playerVelocity.SetDirectionalInput(horizontalDirection);
+
+        if (horizontalDirection.y > 0)
+            playerVelocity.OnJumpInputDown();
+
+        if (horizontalDirection.y < 0)
+            playerVelocity.OnFallInputDown();
+    }
+
+    public void OnKeyJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            playerVelocity.OnJumpInputDown();
+    }
 }
